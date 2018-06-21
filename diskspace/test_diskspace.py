027843337008a0977ff315b728ab35f39d1cb6dc
augustovilarins <augusto.vilarins@gmail.com>
2018-06-21 19:47:05 -0300

    adding tests for test_bytes method

--- diskspace/test_diskspace.py
+++ diskspace/test_diskspace.py
@@ -4,7 +4,7 @@ import unittest
 from diskspace import diskspace
 
 class TestDiskspaceMethods(unittest.TestCase):
-                
+
     def test_subprocess_check_output(self):
         abs_directory = os.path.abspath('')
         cmd = 'du -d 1 ' + abs_directory
@@ -13,6 +13,11 @@ class TestDiskspaceMethods(unittest.TestCase):
         command = subprocess_check_output('du -d 1 {}'.format(abs_directory))
 
         self.assertEqual(command, result)
+
+    def test_bytes_to_readable(self):
+        command = bytes_to_readable(256)
+        self.assertEqual(command, '128.00Kb')
+        self.assertNotEqual(command, '256.00Kb')
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDiskspaceMethods)
